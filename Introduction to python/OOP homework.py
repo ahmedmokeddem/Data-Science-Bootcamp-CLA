f832be542c21ed0c6137a05372328093b956ebec
ahmedmokeddem <63117505+ahmedmokeddem@users.noreply.github.com>
2021-12-11 14:54:36 +0100

    OOP homework

---
+++ Introduction to python/OOP homework.py
@@ -0,0 +1,14 @@
+class Rectangle:
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length*self.width
+class Vehicle:
+    def __init__(self,max_speed,mileage):
+        self.max_speed = max_speed
+        self.mileage =mileage
+class Vehicle:
+    pass
+class Bus (Vehicle):
+    pass
